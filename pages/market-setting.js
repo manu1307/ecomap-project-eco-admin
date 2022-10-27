0bea85a082a6a790d9e81b1a0196d465cbf7638c
manu1307 <jasonsc@naver.com>
2022-10-27 17:42:29 +0900

    배포 테스트

--- pages/market-setting.js
+++ pages/market-setting.js
@@ -1,5 +1,5 @@
 import Layout from "../components/UI/Layout/Layout";
 
 export default function MarketSetting() {
-  return <Layout>MarketSetting</Layout>;
+	return <Layout sideItems={["매장 설정"]}>MarketSetting</Layout>;
 }
