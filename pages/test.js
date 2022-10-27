a826e26d2830f5f4b204cf7e117f3b9380171fd3
manu1307 <jasonsc@naver.com>
2022-10-27 17:35:21 +0900

    배포 테스트

--- pages/test.js
+++ pages/test.js
@@ -1,51 +1,51 @@
-import { atom, useRecoilState } from "recoil";
-import { Line } from "react-chartjs-2";
+// import { atom, useRecoilState } from "recoil";
+// import { Line } from "react-chartjs-2";
 
-const fontsizeState = atom({
-  key: "fontsizeState",
-  default: 14,
-});
+// const fontsizeState = atom({
+//   key: "fontsizeState",
+//   default: 14,
+// });
 
-export const options = {
-  responsive: true,
-  plugins: {
-    title: {
-      display: true,
-      text: "Chart.js Line Chart",
-    },
-  },
-};
+// export const options = {
+//   responsive: true,
+//   plugins: {
+//     title: {
+//       display: true,
+//       text: "Chart.js Line Chart",
+//     },
+//   },
+// };
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: [0, 16, 26, 27, 2, 5, 7],
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-  ],
-};
+// export const data = {
+//   labels,
+//   datasets: [
+//     {
+//       label: "Dataset 1",
+//       data: [0, 16, 26, 27, 2, 5, 7],
+//       borderColor: "rgb(255, 99, 132)",
+//       backgroundColor: "rgba(255, 99, 132, 0.5)",
+//     },
+//   ],
+// };
 
-const FontButton = () => {
-  const [fontsize, setFontSize] = useRecoilState(fontsizeState);
+// const FontButton = () => {
+//   const [fontsize, setFontSize] = useRecoilState(fontsizeState);
 
-  return (
-    <button
-      onClick={() => setFontSize((size) => size + 1)}
-      style={{ fontSize: fontsize }}
-    >
-      click to enlarge
-    </button>
-  );
-};
+//   return (
+//     <button
+//       onClick={() => setFontSize((size) => size + 1)}
+//       style={{ fontSize: fontsize }}
+//     >
+//       click to enlarge
+//     </button>
+//   );
+// };
 
-export default function test() {
-  return (
-    <>
-      <FontButton />;
-      <Line options={options} data={data} />
-    </>
-  );
-}
+// export default function test() {
+//   return (
+//     <>
+//       <FontButton />;
+//       <Line options={options} data={data} />
+//     </>
+//   );
+// }
